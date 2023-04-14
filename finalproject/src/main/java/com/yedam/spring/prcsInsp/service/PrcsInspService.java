a366aacf87ac92d32c736bbe680c507ac2d74019
goodbbak <wndks8564@naver.com>
2023-04-14 14:22:08 +0900

    14:22

--- finalproject/src/main/java/com/yedam/spring/prcsInsp/service/PrcsInspService.java
+++ finalproject/src/main/java/com/yedam/spring/prcsInsp/service/PrcsInspService.java
@@ -5,10 +5,16 @@ import java.util.List;
 public interface PrcsInspService {
 	// 모달용 검사 전체조회
 	public List<PrcsInspVO> selectPrcsList();
-	
-	//공정별 검사기준
+
+	// 공정별 검사기준
 	public List<PrcsInspVO> selectPrcsStd(PrcsInspVO prcsCd);
 
-	// 최대 완제품LOT번호
-	public PrcsInspVO selectMaxEdctsLotNo();
+	// 공정검사완료
+	public int insertDtl(PrcsInspVO prcsInspVO);
+
+	// 공정검사완료 불량등록 / 불량,검사완료 업데이트
+	public int inferUpdateInsert(PrcsInspVO prcsInspVO);
+	
+	// 맥스검사완료코드
+	public PrcsInspVO selectMaxChkCd();
 }
